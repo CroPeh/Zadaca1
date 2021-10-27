3373952b45bbebd3060a0de589ad20c2721cd11b
CroPeh <fran-hep@hotmail.com>
2021-10-27 23:49:55 +0200

    Prva zadaca
    Signed-off-by: CroPeh <fran-hep@hotmail.com>

--- zad2.php
+++ zad2.php
@@ -2,9 +2,11 @@
     $prva=htmlspecialchars($_GET["prvi_kolokvij"]);
     $druga=htmlspecialchars($_GET["drugi_kolokvij"]);
     $rezultat=($prva + $druga)/2;
-    $rezultat=round($rezultat);
+    $zaokruzeni=round($rezultat);
     if($prva == 1 || $druga == 1){
         $rezultat=1;
+        $zaokruzeni=1;
     }
     echo "<p>Ocjena prvog kololkvija je $prva, a drugog $druga</p>";
-    echo "Ukupna ocjena je: $result";
+    echo "<p>Prosjek ocjena iz kolokvija je $rezultat</p>";    
+    echo "Ukupna ocjena je: $zaokruzeni";
